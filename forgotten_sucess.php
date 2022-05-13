4f6ceae643e0facbca333e3796d3637f5b71d860
Henrico Pelozato Maeda <83317236+nyanniyui@users.noreply.github.com>
2022-05-13 19:17:06 -0300

    Delete forgotten_sucess.php

--- forgotten_sucess.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once("templates/header.php");
-	require_once("models/Message.php");
-	
-	$message -> setMessage("Código de segurança enviado para o e-mail!", "success", "forgotten.php");
-?>
