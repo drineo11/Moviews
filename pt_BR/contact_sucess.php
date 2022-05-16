f4aa4d7ad31b7205366491bef22eaddb174b0f8f
Henrico Pelozato Maeda <83317236+nyanniyui@users.noreply.github.com>
2022-05-16 03:16:40 -0300

    Delete pt_BR directory

--- pt_BR/contact_sucess.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once("templates/header.php");
-	require_once("models/Message.php");
-	require_once("models/User.php");
-	require_once("dao/UserDAO.php");
-	
-	$message -> setMessage("Sua mensagem foi enviada com sucesso!", "success", "index.php");
-	$userData = $userDao -> verifyToken(true);
-?>
