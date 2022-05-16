4deb21ce2bc80eed6a4a032d036f4f514d982588
Henrico Pelozato Maeda <83317236+nyanniyui@users.noreply.github.com>
2022-05-16 03:17:04 -0300

    Delete globals.php

--- globals.php
+++
@@ -1,6 +0,0 @@
-<?php
-	// Inicia uma nova sessão ou resume uma sessão existente.
-	session_start();
-	
-	// A base da URL para incluir e acessar outras páginas.
-	$BASE_URL = "http://" . $_SERVER["SERVER_NAME"] . dirname($_SERVER["REQUEST_URI"]."?") . "/";
